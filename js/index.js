37e144c30be3c501d06339386b33de36af602327
squarerobin <squarerobin.82@gmail.com>
2019-12-07 19:05:28 +0100

    mouseenter, mouseleave,mouseover,mouseout done

--- js/index.js
+++ js/index.js
@@ -1,44 +1,46 @@
 // Your code goes here
+document.querySelector("header").style.zIndex = "2";
 
 // mouseenter and mouseleave
 
-let navLink = document.querySelectorAll(".nav-link");
+let navLink = document.querySelectorAll("a.nav-link");
 navLink.forEach(
-    function(link){
-        link.addEventListener('mouseenter', () => {
-            console.log(navLink);
-            console.log("Mouse has entered a nav link");
+    function (link) {
+        link.addEventListener('mouseover', () => {
+            //console.log("Mouse has entered a nav link");
+            link.style.textDecoration = "underline orange 2px";
         });
-        
+
         // mouseleaves
-        link.addEventListener('mouseleave', () => {
-           console.log(navLink);
-           console.log("Mouse has left a nav link");
+        link.addEventListener('mouseout', () => {
+            //console.log("Mouse has left a nav link");
+            link.style.textDecoration = "none";
+
         });
     }
 );
 
 
 
-// mouseleaves
-navLink.addEventListener('mouseleave', () => {
-   console.log(navLink);
-   console.log("Mouse has left a nav link");
-});
-
 // transform - scale images onmouseover
- let image = document.querySelectorAll(".image-content img");
-  image.forEach(
-      function(image){
-        image.addEventListener('onmouseover', () => {
-            console.log("image");
-       
-       });
 
-      }
-  );
- image.addEventListener('onmouseover', () => {
-     console.log("image");
 
-});
+let images = document.querySelectorAll(".img-content img");
+images.forEach(
+    function (image) {
+        image.addEventListener('mouseenter', () => {
+            //console.log("onmouseenter");
+            image.style.transform = "scale(1.1)";
+            image.style.transition = "transform 0.3s";
+            
 
+        });
+        image.addEventListener('mouseleave', () => {
+            //console.log("onmouseleave");
+            image.style.transform = "scale(1)";
+            image.style.transition = "transform 0.3s";
+
+        });
+
+    }
+);
